c1fa471224aa7a0d46d0acb5109251f2291cdf80
theisolatedmonk <96rahulseth@gmail.com>
2023-04-10 21:35:27 +0530

    graphbar function imported

---
+++ src/components/GraphaBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface GraphaBarType {
+  day: string;
+}
+
+export function GraphaBar(props: GraphaBarType) {
+  return (
+    <div className="flex items-center flex-col h-44 w-14 justify-end transition-all gap-2 group ">
+      <div
+        id="mon_amt"
+        className=" value bg-stone-800 items-center justify-center rounded-sm h-6 w-full text-white  group-hover:flex hover:bg-rose-400 "
+      >
+        123
+      </div>
+      <div
+        id="mon1"
+        className="graphcolrd w-10 bg-orange-500 h-full rounded-sm  hover:bg-rose-400"
+      />
+      <div id="mon2">{props.day}</div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,47 @@
-
-import {GraphaBar} from '@/component/GraphaBar'
-import { Logo } from '@/component/Logo'
-
-
+import { GraphaBar } from "@/components/GraphaBar";
+import { Logo } from "@/components/Logo";
 
 export default function Home() {
   return (
-    <main className='flex items-center justify-center min-h-screen'>
-       <div className="container  h-[430px] w-[420px]  flex flex-col justify-between">
-    <div className="contain1 flex justify-between items-center bg-orange-600 h-24 rounded-xl px-4">
-      <div className="txtbalence flex flex-col px-4">
-        <p className="mybal text-sm text-white ">My balance</p>
-        <h1 className="font-bold text-white text-xl">$921.48</h1>
-      </div>
-      <div id="logobox" className="h-16 w-16 flex items-center"><Logo></Logo></div>
-    </div>
-    <div className="contain2 bg-white h-80 gap-2 justify-between rounded-xl flex flex-col p-4  text-yellow-950">
-        <h1 className=" font-bold text-xl ">Spending - Last 7 days</h1>
-        <div className="bardayval flex h-48 items-center text-sm text-center transition-all ">
-        <GraphaBar/>
-         
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="container  h-[430px] w-[420px]  flex flex-col justify-between">
+        <div className="contain1 flex justify-between items-center bg-orange-600 h-24 rounded-xl px-4">
+          <div className="txtbalence flex flex-col px-4">
+            <p className="mybal text-sm text-white ">My balance</p>
+            <h1 className="font-bold text-white text-xl">$921.48</h1>
+          </div>
+          <div id="logobox" className="h-16 w-16 flex items-center">
+            <Logo></Logo>
+          </div>
+        </div>
+        <div className="contain2 bg-white h-80 gap-2 justify-between rounded-xl flex flex-col p-4  text-yellow-950">
+          <h1 className=" font-bold text-xl ">Spending - Last 7 days</h1>
+          <div className="bardayval flex h-48 items-center text-sm text-center transition-all ">
+            <GraphaBar day="mon" />
+            <GraphaBar day="tue" />
+            <GraphaBar day="wed" />
+            <GraphaBar day="thu" />
+            <GraphaBar day="fri" />
+            <GraphaBar day="sat" />
+            <GraphaBar day="sun" />
           </div>
           <hr />
-        <div className="totalamtpercent flex justify-between">
-          <div className="txtamttotal flex flex-col">
-            <p className="text-sm">Total this month</p>
-            <h1 id="totalamt" className="text-3xl font-bold">$478.33</h1>
-          </div>
-          <div className="percenttxt flex flex-col text-right">
-            <p id="percent" className="text-sm font-bold">+2.4%</p>
-            <p className="text-sm">for last month</p>
+          <div className="totalamtpercent flex justify-between">
+            <div className="txtamttotal flex flex-col">
+              <p className="text-sm">Total this month</p>
+              <h1 id="totalamt" className="text-3xl font-bold">
+                $478.33
+              </h1>
+            </div>
+            <div className="percenttxt flex flex-col text-right">
+              <p id="percent" className="text-sm font-bold">
+                +2.4%
+              </p>
+              <p className="text-sm">for last month</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </main>
-
- 
-  )
+  );
 }
