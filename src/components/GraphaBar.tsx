499b708d2b2d41f2e6534a8805f162a1415213a4
theisolatedmonk <96rahulseth@gmail.com>
2023-04-10 22:53:00 +0530

    data added and current day

--- src/components/GraphaBar.tsx
+++ src/components/GraphaBar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 interface GraphaBarType {
   day: string;
+  amount: number;
+  currentDay: string;
 }
 
 export function GraphaBar(props: GraphaBarType) {
@@ -9,12 +11,19 @@ export function GraphaBar(props: GraphaBarType) {
     <div className="flex items-center flex-col h-44 w-14 justify-end transition-all gap-2 group ">
       <div
         id="mon_amt"
-        className=" value bg-stone-800 items-center justify-center rounded-sm h-6 w-full text-white  group-hover:flex hidden">
-        123
+        className=" value bg-stone-800 items-center justify-center rounded-sm h-6 w-full text-white  group-hover:flex hidden"
+      >
+        {props.amount}
       </div>
       <div
+        style={{
+          height: `${props.amount}%`,
+        }}
         id="mon1"
-        className="graphcolrd w-10 bg-orange-500 h-full rounded-sm  hover:bg-rose-400"
+        className={`graphcolrd w-10   rounded-sm  hover:bg-rose-400  ${
+          props.currentDay === props.day ? "bg-purple-400" : "bg-orange-500"
+        }
+         `}
       />
       <div id="mon2">{props.day}</div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,61 @@
 import { GraphaBar } from "@/components/GraphaBar";
 import { Logo } from "@/components/Logo";
+import expenceData from "@/data/data.json";
 
 export default function Home() {
+  console.log(expenceData);
+  const date = new Date();
+  const currentDay = String(date).slice(0, 3).toLowerCase();
+
   return (
-    <main className="flex items-center justify-center min-h-screen">
+    <main className="flex items-center justify-center min-h-screen bg-[hsl(27,66%,92%)]">
       <div className="container  h-[430px] w-[420px]  flex flex-col justify-between">
-      <Balance/>
-       
-        <div className="contain2 bg-white h-80 gap-2 justify-between rounded-xl flex flex-col p-4  text-yellow-950">
+        <Balance />
+
+        <div className="contain2 bg-[hsl(33,100%,98%)] h-80 gap-2 justify-between rounded-xl flex flex-col p-4  text-yellow-950">
           <h1 className=" font-bold text-xl ">Spending - Last 7 days</h1>
           <div className="bardayval flex h-48 items-center text-sm text-center transition-all ">
-            <GraphaBar day="mon" />
-            <GraphaBar day="tue" />
-            <GraphaBar day="wed" />
-            <GraphaBar day="thu" />
-            <GraphaBar day="fri" />
-            <GraphaBar day="sat" />
-            <GraphaBar day="sun" />
+            {expenceData.map(({ amount, day }) => (
+              <GraphaBar currentDay={currentDay} amount={amount} day={day} />
+            ))}
           </div>
           <hr />
-          <TotalThisMonth/>
+          <TotalThisMonth />
         </div>
       </div>
     </main>
   );
 }
 
-
-
-
-function Balance(){
-  return <div className="contain1 flex justify-between items-center bg-orange-600 h-24 rounded-xl px-4">
-  <div className="txtbalence flex flex-col px-4">
-    <p className="mybal text-sm text-white ">My balance</p>
-    <h1 className="font-bold text-white text-xl">$921.48</h1>
-  </div>
-  <div id="logobox" className="h-16 w-16 flex items-center">
-    <Logo></Logo>
-  </div>
-</div>
+function Balance() {
+  return (
+    <div className="contain1 flex justify-between items-center bg-orange-600 h-24 rounded-xl px-4">
+      <div className="txtbalence flex flex-col px-4">
+        <p className="mybal text-sm text-white ">My balance</p>
+        <h1 className="font-bold text-white text-xl">$921.48</h1>
+      </div>
+      <div id="logobox" className="h-16 w-16 flex items-center">
+        <Logo></Logo>
+      </div>
+    </div>
+  );
 }
 
-function TotalThisMonth(){
-  return <div className="totalamtpercent flex justify-between">
-  <div className="txtamttotal flex flex-col">
-    <p className="text-sm">Total this month</p>
-    <h1 id="totalamt" className="text-3xl font-bold">
-      $478.33
-    </h1>
-  </div>
-  <div className="percenttxt flex flex-col text-right">
-    <p id="percent" className="text-sm font-bold">
-      +2.4%
-    </p>
-    <p className="text-sm">for last month</p>
-  </div>
-</div>
+function TotalThisMonth() {
+  return (
+    <div className="totalamtpercent flex justify-between">
+      <div className="txtamttotal flex flex-col">
+        <p className="text-sm">Total this month</p>
+        <h1 id="totalamt" className="text-3xl font-bold">
+          $478.33
+        </h1>
+      </div>
+      <div className="percenttxt flex flex-col text-right">
+        <p id="percent" className="text-sm font-bold">
+          +2.4%
+        </p>
+        <p className="text-sm">for last month</p>
+      </div>
+    </div>
+  );
 }
